8836c6d595822e9e91abe391a1da5f101200f1ae
DanJFletcher <dannyjfletcher@outlook.com>
2017-06-15 23:03:29 -0400

    Getting images from dropbox and showing in lightbox

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 import Dropbox from 'dropbox';
+import LightBox from 'lightbox2';
 
 const dbx = new Dropbox(
     {
         accessToken: 'VMxqipghP2AAAAAAAAAAXQNBau2yjzg236l8yUzfGRGgd3rqtrZ4tf4wyViQCwle'
     });
 
-dbx.filesDownload({path: 'apps/keepingupwiththehoffmans/glasses_hero.jpg'})
+dbx.filesListFolder({path: '/apps/keepingupwiththehoffmans'})
   .then(function(response) {
     console.log(response);
-    var img = document.createElement('img');
-    img.src=window.URL.createObjectURL(response.fileBlob);
-    document.body.appendChild(img);
+    response.entries.forEach(function(entry) {
+      console.log(entry.name);
+      dbx.filesGetTemporaryLink({
+        path: '/apps/keepingupwiththehoffmans/' + entry.name
+      })
+        .then(function(response) {
+          console.log(response);
+
+          let img = document.createElement('img');
+          img.src = response.link;
+          img.className = 'thumbnail'
+
+          let anchor = document.createElement('a');
+          anchor.href = img.src;
+          anchor.dataset.lightbox = "gallery";
+
+          // select the modal body
+          let modalBody = document.getElementById("gallery-modal-body");
+          modalBody.appendChild(anchor);
+          anchor.appendChild(img);
+        })
+        .catch(function(error) {
+          console.log(error);
+        })
+    });
   })
   .catch(function(error) {
     console.log(error);
