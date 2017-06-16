59742e01d6291d9cf6c85850a4c83a4a23bce388
DanJFletcher <dannyjfletcher@outlook.com>
2017-06-15 21:36:42 -0400

    WIP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("oh hey");
+import Dropbox from 'dropbox';
+
+const dbx = new Dropbox(
+    {
+        accessToken: 'VMxqipghP2AAAAAAAAAAXQNBau2yjzg236l8yUzfGRGgd3rqtrZ4tf4wyViQCwle'
+    });
+
+dbx.filesDownload({path: 'apps/keepingupwiththehoffmans/glasses_hero.jpg'})
+  .then(function(response) {
+    console.log(response);
+    var img = document.createElement('img');
+    img.src=window.URL.createObjectURL(response.fileBlob);
+    document.body.appendChild(img);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
